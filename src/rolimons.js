2041d1e4e561479626824c324a8ba1249294d48f
junaga <hermann-stanew@invita.gmbh>
2025-01-26 22:54:28 +0100

    another example

---
+++ src/rolimons.js
@@ -0,0 +1,25 @@
+import { sleep } from "../lib.js"
+import { z } from "zod"
+
+/**
+ * @param {import("@browserbasehq/stagehand").Page} page 
+ */
+export async function run(page) {
+  await page.goto("https://www.rolimons.com/catalog")
+  await page.act("Accept all cookies")
+
+  const items = await page.extract({
+    instruction: "titles of all the game items on the page",
+    schema: z.object({ array: z.array(z.string()) }),
+  })
+
+  await page.act("click on the second page")
+  const items2 = await page.extract({
+    instruction: "titles of all the game items on the page",
+    schema: z.object({ array: z.array(z.string()) }),
+  })
+
+  console.log({ items, items2 })
+
+  await page.close()
+}
