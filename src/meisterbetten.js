ca376cf527560b19e03c8793f7e0858b022c0645
junaga <hermann-stanew@invita.gmbh>
2025-01-27 08:28:58 +0100

    crawling another site

--- src/meisterbetten.js
+++ src/meisterbetten.js
@@ -1,15 +1,15 @@
 import { goto } from "./lib/main.js"
 /** @typedef {import("@browserbasehq/stagehand").Page} Page */
 
-function query() {
-  const section = "body > div.elementor.elementor-83960.elementor-location-archive.product > div.elementor-element.elementor-element-db5d353.e-flex.e-con-boxed.e-con.e-parent.e-lazyloaded > div > div > div > section > article"
-  const products = [...document.querySelectorAll(section)]
+function queryProducts() {
+  const selector = "body > div.elementor.elementor-83960.elementor-location-archive.product > div.elementor-element.elementor-element-db5d353.e-flex.e-con-boxed.e-con.e-parent.e-lazyloaded > div > div > div > section > article"
+  const products = [...document.querySelectorAll(selector)]
   const data = []
 
   for (const product of products) {
     data.push({
-      img: product.querySelector(".wpr-grid-image-wrap > img").src,
       name: product.querySelector("h2 a").innerHTML,
+      img: product.querySelector(".wpr-grid-image-wrap > img").src,
       url: product.querySelector("h2 a").href,
       price: product.querySelector(".woocommerce-Price-amount").innerText
     })
@@ -18,25 +18,43 @@ function query() {
   return data
 }
 
+function queryFabrics() {
+  const selector = ".wpforms-field.wpforms-field-checkbox.stoffmuster.wpforms-list-inline.wpforms-conditional-field.wpforms-conditional-show ul li"
+  const products = [...document.querySelectorAll(selector)]
+  const data = []
+
+  for (const product of products) {
+    data.push({
+      name: product.querySelector("li > label > span:last-child").innerText,
+      img: product.querySelector("img").src
+    })
+  }
+
+  return data
+}
+
 /** @param {import("@browserbasehq/stagehand").Page} page */
 export async function run(page) {
+  await goto(page, "https://meisterbetten.de/")
+  await page.act("Accept all cookies")
+
+  // Stoffmuster
+  await goto(page, "https://meisterbetten.de/stoffmuster/")
+  const fabrics = await page.evaluate(queryFabrics)
+
+  // Betten
+  const beds = []
   const urls = [
     "https://meisterbetten.de/alle-modelle/",
     "https://meisterbetten.de/alle-modelle/page/2/",
     "https://meisterbetten.de/alle-modelle/page/3/",
     "https://meisterbetten.de/alle-modelle/page/4/"
   ]
-
-  await goto(page, urls[0])
-  await page.act("Accept all cookies")
-
-  const data = []
   for (const url of urls) {
     await goto(page, url)
-    const products = await page.evaluate(query)
-
-    data.push(...products)
+    const chunk = await page.evaluate(queryProducts)
+    beds.push(...chunk)
   }
 
-  return data
+  return { fabrics, beds }
 }
